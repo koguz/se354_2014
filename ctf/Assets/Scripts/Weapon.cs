2d3590dfc7191a540aed0b50b2f99d4413b61a6c
Kaya Oğuz <kayaoguz@gmail.com>
2014-04-10 00:59:06 +0300

    Minor
    Minor update.

--- ctf/Assets/Scripts/Weapon.cs
+++ ctf/Assets/Scripts/Weapon.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Weapon { 
 	public string name = "Machine Gun";
 	public int ammoCount = int.MaxValue;
-	public float ammoPerSec= 0.05f; // 1
-	public int damPerAmmo= 66; // 1
+	public float ammoPerSec= 1; //0.05f; // 1
+	public int damPerAmmo= 1; //66; // 1
 	public float lastFired = Time.time;
 }
